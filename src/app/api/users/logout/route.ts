b7052642254037cbce04e5576e89af4b8b8d36fe
chtahir797 <chtahir797@gmail.com>
2024-08-16 00:56:45 +0500

    fix logout issue

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -11,7 +11,9 @@ export async function GET(request: NextRequest) {
 
         response.cookies.set("token", "", {
             httpOnly: true,
-            expires: new Date(0)
+            expires: new Date(0), // Set expiration date to a past date
+            path: '/signin',
+            domain: String(process.env.DOMAIN) 
         });
 
         return response;
